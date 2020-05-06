a21cafb77c789162dc3ac54d1265a3bb3a1c2fdd
Ismaele Giuseppe Recca <sc19igr@leeds.ac.uk>
2020-05-06 11:32:31 +0200

    Created base GameObject, and updated the CMakeLists.txt accordingly.

---
+++ src/gameobject.c
@@ -0,0 +1,85 @@
+//
+// Created by IGRec on 6 May 2020.
+//
+
+#include "gameobject.h"
+#include "raymath.h"
+#include <stdlib.h>
+
+static GameObject blankObject;
+static GameObject objectPool[POOL_SIZE];
+
+void InitObjectSystem()
+{
+	blankObject.id = -1;
+	blankObject.position = Vector2Zero();
+	blankObject.updateBehaviour = NULL;
+	blankObject.renderBehaviour = NULL;
+	blankObject.currentState = GO_DESTROYED;
+	
+	for (int i = 0; i < POOL_SIZE; i++)
+	{
+		objectPool[i] = blankObject;
+	}
+}
+
+GameObject* GetObjectByID(int id)
+{
+	return &objectPool[id];
+}
+
+static int GetNextFreeSlot()
+{
+	for (int i = 0; i < POOL_SIZE; i++)
+	{
+		if (objectPool[i].currentState == GO_DESTROYED)
+			return i;
+	}
+	
+	return -1;
+}
+
+GameObject* CreateObject(Vector2 position, void (*updateBehaviour)(GameObject*), void (*renderBehaviour)(GameObject*), void* startFlags, int sizeOfData)
+{
+	int nextID = GetNextFreeSlot();
+	if (nextID == -1)
+		return NULL;
+	
+	GameObject newObject = blankObject;
+	newObject.position = position;
+	newObject.updateBehaviour = updateBehaviour;
+	newObject.renderBehaviour = renderBehaviour;
+	newObject.currentState = GO_ACTIVE;
+	
+	if (sizeOfData > 0)
+		newObject.objectData = malloc(sizeOfData);
+	else
+		newObject.objectData = NULL;
+	
+	objectPool[nextID] = newObject;
+	return &objectPool[nextID];
+}
+
+void UpdateObjects()
+{
+	for (int i = 0; i < POOL_SIZE; i++)
+	{
+		GameObject* object = GetObjectByID(i);
+		if (object->updateBehaviour != NULL && object->currentState == GO_ACTIVE)
+		{
+			object->updateBehaviour(object);
+		}
+	}
+}
+
+void RenderObjects()
+{
+	for (int i = 0; i < POOL_SIZE; i++)
+	{
+		GameObject* object = GetObjectByID(i);
+		if (object->renderBehaviour != NULL && object->currentState == GO_ACTIVE)
+		{
+			object->renderBehaviour(object);
+		}
+	}
+}
---
+++ src/gameobject.h
@@ -0,0 +1,39 @@
+//
+// Created by IGRec on 6 May 2020.
+//
+
+#include "raylib.h"
+
+#ifndef RAYLIB_TEMPLATE_GAMEOBJECT_H
+#define RAYLIB_TEMPLATE_GAMEOBJECT_H
+
+#define POOL_SIZE 2048
+
+typedef enum gameObjectStateEnum
+{
+	GO_DESTROYED = -1,
+	GO_INACTIVE,
+	GO_ACTIVE
+} GameObjectState;
+
+typedef struct gameObjectStruct
+{
+	int id;
+	Vector2 position;
+	GameObjectState currentState;
+	
+	void (*updateBehaviour)(struct gameObjectStruct*);
+	void (*renderBehaviour)(struct gameObjectStruct*);
+	
+	void* objectData;
+} GameObject;
+
+void InitObjectSystem();
+
+GameObject* GetObjectByID(int id);
+GameObject* CreateObject(Vector2 position, void (*updateBehaviour)(GameObject*), void (*renderBehaviour)(GameObject*), void* startFlags, int sizeOfData);
+
+void UpdateObjects();
+void RenderObjects();
+
+#endif //RAYLIB_TEMPLATE_GAMEOBJECT_H
