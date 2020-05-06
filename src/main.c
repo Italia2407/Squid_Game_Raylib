5a1981b134ff7f4e952c3c8134ba4407db7edeaf
Ismaele Giuseppe Recca <sc19igr@leeds.ac.uk>
2020-05-06 10:26:33 +0200

    Set-up main file for the purpose of the project.

--- src/main.c
+++ src/main.c
@@ -1,20 +1,18 @@
 #include "raylib.h"
 
-#define SCREEN_WIDTH (800)
-#define SCREEN_HEIGHT (450)
+#define SCREEN_WIDTH (1280)
+#define SCREEN_HEIGHT (720)
+#define FPS_CAP (60)
 
-// Change this depending on the path of your executable relative to the assets folder
-#define ASSET_PATH "assets/"
+// The default path to all of our assets
+#define ASSET_PATH "../assets/"
 
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Window title");
-
-    Texture2D texture = LoadTexture(ASSET_PATH"test.png");
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Squid Game");
+    SetTargetFPS(FPS_CAP);               // Set our game to run at 60 frames-per-second
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -27,16 +25,9 @@ int main(void)
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
-
-        const char* text = "OMG! IT WORKS!";
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+        ClearBackground(SKYBLUE);	// Set background colour to
+        
+        
 
         EndDrawing();
         //----------------------------------------------------------------------------------
