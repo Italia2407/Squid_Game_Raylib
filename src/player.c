f314566620a21a68ebac0728d7bf7935b9073e23
Ismaele Giuseppe Recca <sc19igr@leeds.ac.uk>
2020-05-24 09:54:56 +0200

    Made a small palette swapper to change colours of the player

--- src/player.c
+++ src/player.c
@@ -98,12 +98,56 @@ void RenderPlayer(GameObject* playerObject)
 	
 	// Main Body
 	//------------------------------------------------------------------------------------------------------------------
-	Texture squidImage = LoadTexture(ASSET_PATH"Inkling_Temp_Sprite.png");
-	Rectangle imageSection = (Rectangle){7, 3, 13, 18};
-	Rectangle squidRectangle = (Rectangle){playerObject->position.x, playerObject->position.y, 52, 72};
-	Vector2 squidCentre = (Vector2){squidRectangle.width / 2, squidRectangle.height / 2};
 	
-	DrawTexturePro(squidImage, imageSection, squidRectangle, squidCentre, squidData->directionAngle, WHITE);
+	// TODO
+	// Set properties to be set only once and updated regularly
+	Image playerImage = LoadImage(ASSET_PATH"Inkling_Temp_Sprite.png");
+	ImageFormat(&playerImage, UNCOMPRESSED_R8G8B8A8);
+	Color* pixels = playerImage.data;
+	int pixel_count = playerImage.width * playerImage.height;
+	
+	int palette_size;
+	Color* palette = ImageExtractPalette(playerImage, 16, &palette_size);
+	Color new_palette[16] = {
+			(Color){147, 255, 5, 255},
+			(Color){208, 255, 120, 255},
+			(Color){2, 232, 14, 255},
+			(Color){68, 65, 64, 255},
+			(Color){255, 255, 255, 255},
+			(Color){0, 217, 105, 255},
+			(Color){0, 0, 0, 0},
+			(Color){0, 0, 0, 0},
+			(Color){0, 0, 0, 0},
+			(Color){0, 0, 0, 0},
+			(Color){0, 0, 0, 0},
+			(Color){0, 0, 0, 0},
+			(Color){0, 0, 0, 0},
+			(Color){0, 0, 0, 0},
+			(Color){0, 0, 0, 0},
+	};
+	
+	for (int y = 0; y < playerImage.height; y++)
+	{
+		for (int x = 0; x < playerImage.width; x++)
+		{
+			Color* pixel = &pixels[y * playerImage.width + x];
+			for (int i = 0; i < palette_size; i++)
+			{
+				if (ColorToInt(palette[i]) == ColorToInt(*pixel))
+				{
+					*pixel = new_palette[i];
+					break;
+				}
+			}
+		}
+	}
+	
+	Texture playerTexture = LoadTextureFromImage(playerImage);
+	Rectangle imageSection = (Rectangle){7, 3, 13, 18};
+	Rectangle playerRectangle = (Rectangle){playerObject->position.x, playerObject->position.y, 52, 72};
+	Vector2 playerCentre = (Vector2){playerRectangle.width / 2, playerRectangle.height / 2};
+	
+	DrawTexturePro(playerTexture, imageSection, playerRectangle, playerCentre, squidData->directionAngle, WHITE);
 	/*
 	// The dimensions of the squid sprite
 	Rectangle squidRectangle = (Rectangle){squidObject->position.x, squidObject->position.y, SQUID_WIDTH, SQUID_HEIGHT};
